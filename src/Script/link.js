9c1b7b9e076bbe5431317b0b6dadb33aa6abd7c3
cMcVay <86023551+cMcVay@users.noreply.github.com>
2022-08-10 14:54:05 -0400

    get rid of script for links

--- src/Script/link.js
+++
@@ -1,8 +0,0 @@
-const projectCard = document.querySelector(".project");
-const projectLink = document.querySelector(".projectLink");
-
-function handleClick(event) {
-    projectLink.click();
-}
-
-projectCard.addEventListener("click", handleClick);
